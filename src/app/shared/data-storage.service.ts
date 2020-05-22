27f467c7cd90b7ce6da0e9a3ab8104b270d81f67
Sascha Dörnath <sascha.doernath@gmx.de>
2020-05-22 22:16:46 +0200

    Added Authentication and route protection

--- src/app/shared/data-storage.service.ts
+++ src/app/shared/data-storage.service.ts
@@ -1,8 +1,9 @@
 import { Injectable } from "@angular/core";
-import { HttpClient } from "@angular/common/http";
+import { HttpClient, HttpParams } from "@angular/common/http";
 import { RecipesService } from "../recipes/recipes.service";
 import { Recipe } from "../recipes/recipe.model";
-import { map, tap } from "rxjs/operators";
+import { map, tap, take, exhaustMap } from "rxjs/operators";
+import { AuthService } from "../auth/auth.service";
 
 @Injectable({
   providedIn: "root",
@@ -12,7 +13,8 @@ export class DataStorageService {
 
   constructor(
     private http: HttpClient,
-    private recipesService: RecipesService
+    private recipesService: RecipesService,
+    private authService: AuthService
   ) {}
 
   storeRecipes() {
